420bc723f8aa1a8678574c6d46e575d3aa9568fd
coromoEXE <corentinpapot2004@gmail.com>
2023-01-25 20:45:09 +0100

    change is_dead

--- src/philo_process.c
+++ src/philo_process.c
@@ -6,7 +6,7 @@
 /*   By: cpapot <cpapot@student.42lyon.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:00:24 by cpapot            #+#    #+#             */
-/*   Updated: 2023/01/25 17:30:36 by cpapot           ###   ########.fr       */
+/*   Updated: 2023/01/25 20:22:27 by cpapot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,10 @@ static void	eat_philo(t_philo *info)
 	info->last_eat = tmp - info->info->creation_time;
 }
 
-static void	fininsh_philo(t_philo *info)
-{
-	pthread_mutex_lock (& info->info->mutex);
-	if (info->info->is_alive != 1)
-	{
-		free(info);
-		pthread_mutex_unlock (& info->info->mutex);
-		exit(EXIT_SUCCESS);
-	}
-	pthread_mutex_unlock (& info->info->mutex);
-}
-
 static void	is_dead(t_philo *info)
 {
 	long			tmp;
 	struct timeval	time;
-
 
 	gettimeofday(&time, NULL);
 	pthread_mutex_lock (& info->info->mutex);
@@ -61,6 +48,12 @@ static void	is_dead(t_philo *info)
 		printf(BLACK"%ld %d is dead\n", tmp, info->actual_philo);
 		info->info->is_alive = 0;
 		free(info->info->fork_tab);
+	}
+	if (info->info->is_alive != 1)
+	{
+		pthread_mutex_unlock (& info->info->mutex);
+		free(info);
+		exit(EXIT_SUCCESS);
 	}
 	pthread_mutex_unlock (& info->info->mutex);
 }
@@ -74,20 +67,19 @@ void	*philo_process(void *p_data)
 	info = (t_philo *)p_data;
 	while (info->eat_count != info->info->nb_philo_eat)
 	{
+		is_dead(info);
 		eat_philo(info);
 		is_dead(info);
-		fininsh_philo(info);
 		gettimeofday(&time, NULL);
 		tmp = (long)(time.tv_usec * 0.001 + time.tv_sec * 1000)
 			- info->info->creation_time;
 		printf(MAGENTA"%ld %d is sleeping\n", tmp, info->actual_philo);
 		usleep(info->info->time_to_sleep * 1000);
+		is_dead(info);
 		gettimeofday(&time, NULL);
 		tmp = (long)(time.tv_usec * 0.001 + time.tv_sec * 1000)
 			- info->info->creation_time;
 		printf(YELLOW"%ld %d is thinking\n", tmp, info->actual_philo);
-		is_dead(info);
-		fininsh_philo(info);
 	}
 	return (p_data);
 }
